2c29d1867b7440d6d192bdbf9e65f5226cd06b38
Isa Santos <isabela.vieir@hotmail.com>
2022-10-24 11:41:44 -0300

    update

---
+++ web/src/pages/Dashboard/js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 0,
+    nav: false,
+    responsive: {
+        0:{
+            items:2.6
+        },
+        500:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
